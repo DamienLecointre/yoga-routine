4ee69db2e52647f744d679338ccc796f3a7296fb
Damien Lecointre <89322001+DamienLecointre@users.noreply.github.com>
2025-05-19 14:16:31 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("hello");
+
+const position = {
+  position0: "0.png",
+  position1: "1.png",
+  position2: "2.png",
+  position3: "3.png",
+  position4: "4.png",
+  position5: "5.png",
+  position6: "6.png",
+  position7: "7.png",
+  position8: "8.png",
+  position9: "9.png",
+};
